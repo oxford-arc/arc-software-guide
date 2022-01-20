5a51025d4ea2dd5b60c4034d599ee697265939db
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2022-01-20 14:43:52 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ARC Software Guide'
+copyright = '2021'
+author = 'The ARC Team'
 
 release = '0.1'
 version = '0.1.0'
